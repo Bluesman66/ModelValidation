f3cfa0d35e198456ddaec2ff55179d7315e59c1a
alex.paclin <alex.paclin@gmail.com>
2016-12-15 23:40:30 +0500

    Явная проверка достоверности модели

--- ModelValidation/Controllers/HomeController.cs
+++ ModelValidation/Controllers/HomeController.cs
@@ -17,9 +17,23 @@ namespace ModelValidation.Controllers
 		[HttpPost]
 		public ViewResult MakeBooking(Appointment appt)
 		{
-			// В реальном приложении здесь находились бы операторы
-			// для сохранения нового объекта Appointment в базе данных
-			return View("Completed", appt);
+			if (String.IsNullOrEmpty(appt.ClientName))
+				ModelState.AddModelError("ClientName", "Введите свое имя");
+
+			if (ModelState.IsValidField("Date") && DateTime.Now > appt.Date)
+				ModelState.AddModelError("Date", "Введите дату относящуюся к будущему");
+
+			if (!appt.TermsAccepted)
+				ModelState.AddModelError("TermsAccepted", "Вы должны принять условия");
+
+			if (ModelState.IsValid)
+			{
+				// В реальном приложении здесь находились бы операторы
+				// для сохранения нового объекта Appointment в базе данных
+				return View("Completed", appt);
+			}
+			else
+				return View();
 		}
 	}
 }
