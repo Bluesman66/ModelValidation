8536a9016d4d5825c2e76d4bc1e6b993dbaf09b8
alex.paclin <alex.paclin@gmail.com>
2016-12-16 16:47:17 +0500

    Определение самопроверяемых моделей

--- ModelValidation/Models/Appointment.cs
+++ ModelValidation/Models/Appointment.cs
@@ -6,17 +6,33 @@ using System.Web.Mvc;
 
 namespace ModelValidation.Models
 {
-	[NoVasyaOnMonday]
-	public class Appointment
-	{
-		[Required(ErrorMessage = "Введите свое имя")]
-		public string ClientName { get; set; }
+	public class Appointment : IValidatableObject
+    {
+        public string ClientName { get; set; }
 
-		[DataType(DataType.Date)]
+        [DataType(DataType.Date)]
 		[FutureDate(ErrorMessage = "Введите дату относящуюся к будущему")]
 		public DateTime Date { get; set; }
-				
-		[MustBeTrue(ErrorMessage = "Вы должны принять условия")]
-		public bool TermsAccepted { get; set; }
-	}
+
+        public bool TermsAccepted { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> errors = new List<ValidationResult>();
+
+            if (String.IsNullOrEmpty(ClientName))
+                errors.Add(new ValidationResult("Введите свое имя"));
+
+            if (DateTime.Now > Date)
+                errors.Add(new ValidationResult("Введите дату относящуюся к будущему"));
+
+            if (errors.Count == 0 && ClientName == "Вася" && Date.DayOfWeek == DayOfWeek.Monday)
+                errors.Add(new ValidationResult("Васи в понедельник отдыхают!"));
+
+            if (!TermsAccepted)
+                errors.Add(new ValidationResult("Вы должны принять условия"));
+
+            return errors;
+        }
+    }
 }
