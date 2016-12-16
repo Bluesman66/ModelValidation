f9bea4a3fa44c421bbcf82dfb8e144b65588b173
alex.paclin <alex.paclin@gmail.com>
2016-12-16 15:39:19 +0500

    Указание правил проверки достоверности с использованием метаданных модели

--- ModelValidation/Controllers/HomeController.cs
+++ ModelValidation/Controllers/HomeController.cs
@@ -17,23 +17,6 @@ namespace ModelValidation.Controllers
 		[HttpPost]
 		public ViewResult MakeBooking(Appointment appt)
 		{
-			if (String.IsNullOrEmpty(appt.ClientName))
-				ModelState.AddModelError("ClientName", "Введите свое имя");
-
-			if (!ModelState.IsValidField("Date"))
-				ModelState.AddModelError("Date", "Введите дату");
-			else if (DateTime.Now > appt.Date)
-				ModelState.AddModelError("Date", "Введите дату относящуюся к будущему");
-
-			if (!appt.TermsAccepted)
-				ModelState.AddModelError("TermsAccepted", "Вы должны принять условия");
-
-			if (ModelState.IsValidField("ClientName") && ModelState.IsValidField("Date")
-				&& appt.ClientName == "Вася" && appt.Date.DayOfWeek == DayOfWeek.Monday)
-			{
-				ModelState.AddModelError("", "Васи в понедельник отдыхают!");
-			}
-
 			if (ModelState.IsValid)
 			{
 				// В реальном приложении здесь находились бы операторы
--- ModelValidation/Models/Appointment.cs
+++ ModelValidation/Models/Appointment.cs
@@ -7,11 +7,14 @@ namespace ModelValidation.Models
 {
 	public class Appointment
 	{
+		[Required(ErrorMessage = "Введите свое имя")]
 		public string ClientName { get; set; }
 
 		[DataType(DataType.Date)]
+		[Required(ErrorMessage = "Введите дату")]
 		public DateTime Date { get; set; }
 
+		[Range(typeof(bool), "true", "true", ErrorMessage = "Вы должны принять условия")]
 		public bool TermsAccepted { get; set; }
 	}
 }
